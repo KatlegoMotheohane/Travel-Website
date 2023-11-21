4eb82b9e25288353a325126e2f0cdd5483ee22f4
Katlego Motheohane <146595863+KatlegoMotheohane@users.noreply.github.com>
2023-11-21 10:50:00 +0200

    Add files via upload

---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../App";
+import HeroSection from "../Herosection";
+import Cards from "../Cards";
+import Footer from "../Footer";
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
